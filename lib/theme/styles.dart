a3456ae1acc5959f707643b02401d8391bec3675
aliwka852-ai <aliwka852@gmail.com>
2026-06-12 06:46:38 +0400

    Create styles.dart

---
+++ lib/theme/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static const TextStyle appTitle = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2,
+    color: Colors.white,
+  );
+
+  static const TextStyle neonLabel = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.neonCyan,
+    letterSpacing: 2,
+  );
+}
