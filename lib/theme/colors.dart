9c0700246a34b10a916b94b3dc70a0882a8d99bc
aliwka852-ai <aliwka852@gmail.com>
2026-06-12 06:45:29 +0400

    Create colors.dart

---
+++ lib/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0B0B10);
+  static const Color neonPurple = Color(0xFFC000FF);
+  static const Color neonCyan = Color(0xFF00E5FF);
+  static const Color neonGreen = Color(0xFF00FF88);
+}
